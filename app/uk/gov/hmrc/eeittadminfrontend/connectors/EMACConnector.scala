83e5120f4db817b45c9a94637954ce41eebd0bd1
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-06-13 15:24:49 +0100

    WIP still requires assign to be fully implemented

--- app/uk/gov/hmrc/eeittadminfrontend/connectors/EMACConnector.scala
+++ app/uk/gov/hmrc/eeittadminfrontend/connectors/EMACConnector.scala
@@ -15,13 +15,33 @@ case class EnrollmentKey(value: String)
 case class KnownFacts(enrollmentKey: EnrollmentKey, verifiers: Verifiers)
 case class Enrollment(userid: String, enrollmentKey: EnrollmentKey)
 
+
 trait EMACConnectorHelper {
 
   val PUT : HttpPut = WSHttp
   val POST : HttpPost = WSHttp
 
+  val allocateInsertJson: String =
+    s"""
+       |{
+       |    "service": "$service",
+       |    "friendlyName": "friendly name",
+       |    "type": "$enrolmentType",
+       |    "verifiers": [
+       |       { "PostCode": "postcode" },
+       |       { "NINO": "123" }
+       |    ]
+       |}
+      """.stripMargin
+
+  //ES6
   def loadKF(knownFacts: KnownFacts) = {
     PUT.PUT[Verifiers, JsValue](s"http://enrolment-store-proxy.protected.mdtp:80/enrolment-store-proxy/enrolment-store/enrolments/${knownFacts.enrollmentKey}", knownFacts.verifiers)
+  }
+
+  //ES8
+  def allocateAnEnrollment() = {
+    POST.POST[](s"http://enrolment-store-proxy.protected.mdtp:80/enrolment-store-proxy/enrolment-store/groups/${groupId}/enrolments/$service~$identifier~$value")
   }
 
   def assignEnrollment(enrollment: Enrollment) = {
