6c54d197600a6386fef97d18bb9d4da29dc43acf
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-06-21 13:16:42 +0100

    putting legacy CredId on correct endpoint

--- app/uk/gov/hmrc/eeittadminfrontend/connectors/EMACConnector.scala
+++ app/uk/gov/hmrc/eeittadminfrontend/connectors/EMACConnector.scala
@@ -119,14 +119,14 @@ trait EMACConnectorHelper {
          |}
       """.stripMargin)
 
-    POST.POST[JsValue, HttpResponse](s"$ES8url${user.groupId}/enrolments/${enrollment.enrollmentKey.service}~${enrollment.enrollmentKey.identifier}~${enrollment.enrollmentKey.value}", allocateInsertJson, Seq("Content-Type" -> "application/json"))
+    POST.POST[JsValue, HttpResponse](s"$ES8url${user.groupId}/enrolments/${enrollment.enrollmentKey.service}~${enrollment.enrollmentKey.identifier}~${enrollment.enrollmentKey.value}?legacy-credentialId=${user.credId}", allocateInsertJson, Seq("Content-Type" -> "application/json"))
   }
 
   //ES11
   def assignEnrollment(enrollment: Enrollment, user: User)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[Option[JsValue]] = {
     allocateAnEnrollment(enrollment, user).map(result).flatMap {
       case None => {
-        POST.POSTEmpty[HttpResponse](s"$ES11url${user.credId}/enrolments/${enrollment.enrollmentKey.service}~${enrollment.enrollmentKey.identifier}~${enrollment.enrollmentKey.value}?legacy-credentialId=${user.credId}").map(result).map{
+        POST.POSTEmpty[HttpResponse](s"$ES11url${user.credId}/enrolments/${enrollment.enrollmentKey.service}~${enrollment.enrollmentKey.identifier}~${enrollment.enrollmentKey.value}").map(result).map{
           case None => None
           case x =>
             Logger.error("Emac Connector returned an error for assign Enrollment")
