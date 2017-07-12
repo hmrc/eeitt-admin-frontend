a82ffc34cb64f873493793975ae76b233e3b02ab
Harrison Baxter <harrison.baxter@digital.hmrc.gov.uk>
2017-07-12 15:02:47 +0100

    Adding the bulk load controller and model(WIP)

---
+++ app/uk/gov/hmrc/eeittadminfrontend/models/KnownFacts.scala
@@ -0,0 +1,7 @@
+package uk.gov.hmrc.eeittadminfrontend.models
+
+case class KnownFacts(
+                       postCode: String,
+                       countryCode: String,
+                       utr: String
+                     )
