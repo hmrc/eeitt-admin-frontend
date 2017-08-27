c0071a86d9c72726520f4f55aad51913345b29ae
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-08-27 12:25:46 +0100

    test of new config

--- app/uk/gov/hmrc/eeittadminfrontend/services/GoogleVerifier.scala
+++ app/uk/gov/hmrc/eeittadminfrontend/services/GoogleVerifier.scala
@@ -17,17 +17,21 @@
 package uk.gov.hmrc.eeittadminfrontend.services
 
 import java.net.{ Authenticator, InetSocketAddress, PasswordAuthentication, Proxy, SocketAddress }
+import java.security.KeyStore
 import java.util.Collections
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
-import com.google.api.client.http.javanet.NetHttpTransport
+import com.google.api.client.http.apache.ApacheHttpTransport
+import com.google.api.client.http.javanet.{ ConnectionFactory, NetHttpTransport }
 import com.google.api.client.http.javanet.NetHttpTransport.Builder
 import com.google.api.client.json.jackson2.JacksonFactory
-import org.apache.http.Header
+import org.apache.http.{ Header, HttpHost }
+import org.apache.http.client.methods.HttpPost
 import org.apache.http.impl.DefaultHttpRequestFactory
 import play.api.Logger
 import play.api.libs.json.Format
+import sun.security.util.SecurityConstants
 import uk.gov.hmrc.eeittadminfrontend.controllers.auth.ClientID
 
 class GoogleVerifier extends GoogleVerifierHelper
@@ -51,8 +55,8 @@ trait GoogleVerifierHelper {
     Authenticator.setDefault(authenticator)
     System.setProperty("http.proxyHost", squid.host)
     System.setProperty("http.proxyPort", squid.port.toString)
-    val proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(squid.host, squid.port))
-    new NetHttpTransport.Builder().setProxy(proxy).doNotValidateCertificate().build()
+    val proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(s"${squid.username}:${squid.password}@${squid.host}", squid.port))
+    new NetHttpTransport.Builder().setProxy(proxy).build()
   } else GoogleNetHttpTransport.newTrustedTransport()
 
   lazy val tokenVerifier: GoogleIdTokenVerifier = new GoogleIdTokenVerifier.Builder(httpTransport, JacksonFactory.getDefaultInstance)
