9cd2e1a30d0172ec9b0ff3f137853138ea69dc41
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-09-29 12:21:53 +0100

    adding in  both implementations and script

--- app/uk/gov/hmrc/eeittadminfrontend/controllers/GformsController.scala
+++ app/uk/gov/hmrc/eeittadminfrontend/controllers/GformsController.scala
@@ -19,12 +19,12 @@ package uk.gov.hmrc.eeittadminfrontend.controllers
 import play.api.Logger
 import play.api.data.Form
 import play.api.data.Forms._
-import play.api.i18n.{I18nSupport, MessagesApi}
-import play.api.libs.json.{JsValue, Json}
+import play.api.i18n.{ I18nSupport, MessagesApi }
+import play.api.libs.json.{ JsValue, Json }
 import uk.gov.hmrc.eeittadminfrontend.AppConfig
 import uk.gov.hmrc.eeittadminfrontend.config.Authentication
 import uk.gov.hmrc.eeittadminfrontend.connectors.GformConnector
-import uk.gov.hmrc.eeittadminfrontend.models.{FormTypeId, GformId}
+import uk.gov.hmrc.eeittadminfrontend.models.{ FormTypeId, GformId }
 import uk.gov.hmrc.play.frontend.auth.Actions
 import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
 import uk.gov.hmrc.play.frontend.controller.FrontendController
@@ -49,7 +49,7 @@ class GformsController(val authConnector: AuthConnector)(implicit appConfig: App
 
   val vat126Template: JsValue = Json.parse("{\n  \"_id\": \"HO10\",\n  \"formName\": \"Hydrocarbon oils\",\n  \"description\": \"\",\n  \"dmsSubmission\": {\n    \"customerId\": \"${auth.payenino}\",\n    \"classificationType\": \"BT-NRU-Environmental\",\n    \"businessArea\": \"FinanceOpsCorpT\"\n  },\n  \"authConfig\":{\n    \"authModule\": \"legacyEEITTAuth\",\n    \"regimeId\": \"GF\"\n  },\n  \"submitSuccessUrl\": \"http://www.google.co.uk\",\n  \"submitErrorUrl\": \"http://www.yahoo.co.uk\",\n  \"sections\": [\n    {\n      \"title\": \"Enter your business details\",\n      \"shortName\": \"Business details\",\n      \"fields\": [\n        {\n          \"id\": \"business-name\",\n          \"type\": \"text\",\n          \"label\": \"Business name\",\n          \"mandatory\": \"true\"\n        },\n        {\n          \"id\": \"telephone-number\",\n          \"type\": \"text\",\n          \"label\": \"Phone number\",\n          \"format\": \"telephoneNumber\"\n        },\n        {\n          \"id\": \"business-address\",\n          \"label\": \"Registered business address\",\n          \"mandatory\": \"true\",\n          \"type\": \"address\",\n          \"international\": \"yes\"\n        }\n      ]\n    },\n    {\n      \"title\": \"Enter the start and end dates for this return\",\n      \"shortName\": \"Start and end dates for this return\",\n      \"fields\": [\n        {\n          \"id\": \"start-date\",\n          \"type\": \"date\",\n          \"label\": \"Start date\",\n          \"mandatory\": \"true\",\n          \"helpText\": \"Example: 10 07 2017\"\n        },\n        {\n          \"id\": \"end-date\",\n          \"type\": \"date\",\n          \"label\": \"End date\",\n          \"mandatory\": \"true\",\n          \"helpText\": \"Example: 10 10 2017\"\n        }\n      ]\n    },\n    {\n      \"title\": \"Upload your return document\",\n      \"fields\": [\n        {\n          \"type\": \"file\",\n          \"id\": \"spreadsheet\",\n          \"label\": \"\"\n        },\n        {\n          \"id\": \"upload-info\",\n          \"type\": \"info\",\n          \"infoText\": \"The document you upload must be:  \\n\\n* less that 5 megabytes (5MB)  \\n* in PDF or JPEG format\",\n          \"label\": \"\"\n        },\n        {\n          \"id\": \"upload-contents-info\",\n          \"type\": \"info\",\n          \"infoType\": \"long\",\n          \"infoText\": \"Content goes here...\",\n          \"label\": \"What your document must contain\"\n        }\n      ]\n    },\n    {\n      \"title\": \"Enter the total amount of your claim\",\n      \"fields\": [\n        {\n          \"id\": \"total-claim\",\n          \"type\": \"text\",\n          \"label\": \"\",\n          \"mandatory\": \"true\",\n          \"format\": \"sterling\"\n        }\n      ]\n    },\n    {\n      \"title\": \"Enter your contact person's details\",\n      \"description\": \"This is the person we will contact if we need to discuss your return.\",\n      \"fields\": [\n        {\n          \"id\": \"name\",\n          \"type\": \"text\",\n          \"label\": \"Full name\",\n          \"mandatory\": \"true\"\n        },\n        {\n          \"id\": \"email\",\n          \"type\": \"text\",\n          \"label\": \"Email address\",\n          \"mandatory\": \"true\",\n          \"format\": \"email\"\n        }\n      ]\n    }\n  ],\n  \"declarationSection\": {\n    \"shortName\": \"Sign and submit your return\",\n    \"title\": \"Sign and submit your return\",\n    \"fields\": [\n      {\n        \"id\": \"declarationNameGroup\",\n        \"type\": \"group\",\n        \"label\": \"Approver's details\",\n        \"helpText\": \"Tell us who is approving this return.\",\n        \"fields\": [\n          {\n            \"id\": \"declarationFullName\",\n            \"type\": \"text\",\n            \"label\": \"Full name\"\n          },\n          {\n            \"id\": \"declarationJobTitle\",\n            \"type\": \"text\",\n            \"label\": \"Job title\",\n            \"helpText\": \"Example: Finance Director\"\n          }\n        ]\n      },\n      {\n        \"id\": \"declarationGroup\",\n        \"type\": \"group\",\n        \"label\": \"Declaration\",\n        \"fields\": [\n          {\n            \"id\": \"declarationText\",\n            \"type\": \"info\",\n            \"label\": \"\",\n            \"infoType\": \"noformat\",\n            \"infoText\": \"By submitting the return, you confirm that the details provided are true and complete.\"\n          }\n        ]\n      }\n    ]\n  },\n  \"acknowledgementSection\": {\n    \"shortName\": \"Acknowledgement Page\",\n    \"title\": \"Acknowledgement Page\",\n    \"fields\": [\n      {\n        \"id\": \"ackPageInfoTop\",\n        \"type\": \"info\",\n        \"format\": \"noformat\",\n        \"label\": \"If you owe duty\",\n        \"infoText\": \"## If you owe duty\\n If you have a Direct Debit set up for this tax, you don't need to do anything. Your payment will be collected automatically.  \\n\\nOtherwise, paying electronically is an easy and secure way to pay. Use the details below and make sure you include your registration number (XZGR 00000 123456) on any payment.  \\n\\nSort code: 08-32-00  \\nAccount number: 12000903  \\nAccount name: HMRC Miscellaneous\"\n      },\n      {\n        \"id\": \"ackPageWarning\",\n        \"type\": \"info\",\n        \"format\": \"important\",\n        \"label\": \"\",\n        \"infoText\": \"Make sure you pay the duty owed or you may face a penalty.\"\n      },\n      {\n        \"id\": \"ackPageInfoBottom\",\n        \"type\": \"info\",\n        \"format\": \"noformat\",\n        \"label\": \"If you need to contact us about your return\",\n        \"infoText\": \"## If you need to contact us about your return\\n Call [Content TBC] or email [TBC], quoting your registration number XZGR 00000 123456.\"\n      }\n    ]\n  }\n}")
 
-  def saveGformSchema = Authentication.async(parse.multipartFormData) { implicit request =>
+  def uploadGformSchema = Authentication.async(parse.multipartFormData) { implicit request =>
     request.body.file("template").map{x =>
       val template = Json.parse(Source.fromFile(x.ref.file).getLines().mkString)
       GformConnector.saveTemplate(template).map { x =>
@@ -57,6 +57,17 @@ class GformsController(val authConnector: AuthConnector)(implicit appConfig: App
         Ok("Saved")
       }
     }.getOrElse(Future.successful(Ok("Failed")))
+  }
+
+  def saveGformSchema = Authentication.async(parse.multipartFormData) { implicit request =>
+    val template = Json.parse(request.body.dataParts("template").mkString)
+    GformConnector.saveTemplate(template).map {
+      x =>
+      {
+        Logger.info(s" ${request.session.get("token").get} saved ID: ${template \ "formTypeId"} version: ${template \ "version"}")
+        Ok("Saved")
+      }
+    }
   }
 
   def getAllTemplates = Authentication.async { implicit request =>
