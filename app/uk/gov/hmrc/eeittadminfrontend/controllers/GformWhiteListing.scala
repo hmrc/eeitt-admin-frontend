91f2de0e62a3956fc901aee2e604c4900df90107
daniel.connelly <danile.connelly@digital.hmrc.gov.uk>
2017-10-04 18:26:33 +0100

    adding in dynamic whitelisting to gform

---
+++ app/uk/gov/hmrc/eeittadminfrontend/controllers/GformWhiteListing.scala
@@ -0,0 +1,8 @@
+package uk.gov.hmrc.eeittadminfrontend.controllers
+
+/**
+  * Created by daniel-connelly on 04/10/17.
+  */
+class GformWhiteListing {
+
+}
--- support/encodeScript.sh
+++ support/encodeScript.sh
@@ -3,4 +3,4 @@
 FILENAME=$1
 STRING=$2
 
-${STRING} | sed -e 's/[$][{]/\\u0024\\u007B/g' -e's/[}]["]/\\u007D"/g' -e 's/[e]/\\u0065/g' -e 's/[t]/\\u0074/g' -e 's/[m]/\\u006D/g' -e 's/[g]/\\u0067/g' -e 's/[(]/\\u0028/g' > ${FILENAME}.json
+cat ${STRING} | sed -e 's/[$][{]/\\u0024\\u007B/g' -e's/[}]["]/\\u007D"/g' -e 's/[e]/\\u0065/g' -e 's/[t]/\\u0074/g' -e 's/[m]/\\u006D/g' -e 's/[g]/\\u0067/g' -e 's/[(]/\\u0028/g' > ${FILENAME}.json
