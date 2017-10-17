dd6a5c5c99145e810ca422d8165e1efd827eea8c
Dave English <dave.english@digital.hmrc.gov.uk>
2017-10-17 16:54:36 +0100

    Update dependency versions post 1.34.0 release

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -13,28 +13,28 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.23.0",
-    "uk.gov.hmrc" %% "play-partials" % "5.3.0",
-    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.26.0",
+    "uk.gov.hmrc" %% "play-partials" % "5.4.0",
+    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.4.0",
     "uk.gov.hmrc" %% "play-config" % "4.3.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "govuk-template" % "5.2.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.13.0",
     "uk.gov.hmrc" %% "play-health" % "2.1.0",
-    "uk.gov.hmrc" %% "play-ui" % "7.2.1",
-    "com.github.pureconfig" %% "pureconfig" % "0.7.2",
+    "uk.gov.hmrc" %% "play-ui" % "7.8.0",
+    "com.github.pureconfig" %% "pureconfig" % "0.8.0",
     "com.google.gdata" % "core" % "1.47.1",
-    "com.google.apis" % "google-api-services-oauth2" % "v2-rev124-1.22.0",
+    "com.google.apis" % "google-api-services-oauth2" % "v2-rev131-1.23.0",
     "org.typelevel" % "cats-core_2.11" % "0.9.0",
-    "com.google.api-client" % "google-api-client" % "1.22.0"
+    "com.google.api-client" % "google-api-client" % "1.23.0"
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
-    "org.scalatest" %% "scalatest" % "3.0.1" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
+    "org.scalatest" %% "scalatest" % "3.0.4" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.8.1" % scope,
+    "org.jsoup" % "jsoup" % "1.10.3" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0-M1" % scope
+    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % scope
   )
 
 }
