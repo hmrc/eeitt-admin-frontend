c0c42fa311ed5c5054e6780eb023b538eb8f163f
Dave English <dave.english@digital.hmrc.gov.uk>
2018-07-06 12:47:04 +0100

    Non-hmrc minor version bumps

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -12,18 +12,18 @@ object FrontendBuild extends Build with MicroService {
     ws,
     "uk.gov.hmrc" %% "frontend-bootstrap" % "8.24.0",
     "uk.gov.hmrc" %% "play-partials" % "6.1.0",
-    "com.github.pureconfig" %% "pureconfig" % "0.7.2",
+    "com.github.pureconfig" %% "pureconfig" % "0.9.1",
     "com.google.gdata" % "core" % "1.47.1",
-    "com.google.apis" % "google-api-services-oauth2" % "v2-rev124-1.22.0",
-    "org.typelevel" % "cats-core_2.11" % "0.9.0",
-    "com.google.api-client" % "google-api-client" % "1.22.0"
+    "com.google.apis" % "google-api-services-oauth2" % "v2-rev137-1.23.0",
+    "org.typelevel" % "cats-core_2.11" % "1.1.0",
+    "com.google.api-client" % "google-api-client" % "1.23.0"
   )
 
   def test(scope: String = "test") = Seq(
     "uk.gov.hmrc" %% "hmrctest" % "3.0.0" % scope,
     "org.scalatest" %% "scalatest" % "3.0.1" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
-    "org.jsoup" % "jsoup" % "1.10.2" % scope,
+    "org.jsoup" % "jsoup" % "1.11.3" % scope,
     "com.typesafe.play" %% "play-test" % PlayVersion.current % scope,
     "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % scope
   )
