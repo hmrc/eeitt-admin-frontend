c8c30158a2ee5b1220264277d5b37c21cd74e1cc
Carl Manson <506508+cmanson@users.noreply.github.com>
2025-08-29 14:40:59 +0100

    GFORMS-3661 - Address most PR bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,15 +8,15 @@ object AppDependencies {
   val jacksonDatabindVersion = "2.16.1"
   val http4sVersion = "0.21.22"
   val circeVersion = "0.13.0"
-  val bootstrapVersion = "9.11.0"
+  val bootstrapVersion = "9.19.0"
 
   val compile = Seq(
     ws,
     "uk.gov.hmrc"              %% "bootstrap-frontend-play-30"   % bootstrapVersion,
     "uk.gov.hmrc"              %% "play-partials-play-30"        % "9.1.0",
     "uk.gov.hmrc"              %% "play-frontend-hmrc-play-30"   % "9.11.0",
-    "uk.gov.hmrc"              %% "internal-auth-client-play-30" % "4.0.0",
-    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-play-30"           % "2.6.0",
+    "uk.gov.hmrc"              %% "internal-auth-client-play-30" % "4.1.0",
+    "uk.gov.hmrc.mongo"        %% "hmrc-mongo-play-30"           % "2.7.0",
     "com.github.pureconfig"    %% "pureconfig"                   % "0.17.6",
     "org.typelevel"            %% "cats-core"                    % "2.10.0",
     "org.julienrf"             %% "play-json-derived-codecs"     % "11.0.0",
@@ -47,7 +47,6 @@ object AppDependencies {
   def test(scope: String = "test") =
     Seq(
       "uk.gov.hmrc"            %% "bootstrap-test-play-30"      % bootstrapVersion    % scope,
-      "org.pegdown"             % "pegdown"                     % "1.6.0"             % scope,
       "org.jsoup"               % "jsoup"                       % "1.17.2"            % scope,
       "org.playframework"      %% "play-test"                   % PlayVersion.current % scope,
       "org.scalatestplus.play" %% "scalatestplus-play"          % "7.0.1"             % scope,
