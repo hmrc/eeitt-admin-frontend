8a95783dac47e1a167507c630d9f3ad75e442025
Pavel Vjalicin <pavelvjalicin@users.noreply.github.com>
2026-01-05 11:24:48 +0000

    GFORMS-3188: Playwright server screenshot test.

--- project/PlaywrightBrowserDownload.scala
+++ project/PlaywrightBrowserDownload.scala
@@ -5,7 +5,7 @@ import sbt.Keys.*
 import scala.sys.process.Process
 
 /** Settings defined in this file are run as part of the dist stage.
-  */
+ */
 object PlaywrightBrowserDownload {
   val playwrightBrowserDownload = TaskKey[Int]("playwright-browser-download")
 
@@ -18,10 +18,10 @@ object PlaywrightBrowserDownload {
 
   val playwrightBrowserDownloadSetting: Seq[sbt.Def.Setting[_]] = Seq(
     playwrightBrowserDownload := {
-
-        Process("npx playwright install --with-deps chromium") #&&
-          Process("mkdir " + (baseDirectory.value / "conf" / "browsers").getAbsolutePath) ###
-          Process(Seq("cp","-R",browserDir ,(baseDirectory.value / "conf" / "browsers").getAbsolutePath )) !
+      Process("npm install --no-save playwright") #&&
+        Process("node node_modules/playwright/cli.js install chromium") #&&
+        Process("mkdir " + (baseDirectory.value / "conf" / "browsers").getAbsolutePath) ###
+        Process(Seq("cp","-R",browserDir ,(baseDirectory.value / "conf" / "browsers").getAbsolutePath )) !
     },
     dist := { dist dependsOn playwrightBrowserDownload }.value
   )
